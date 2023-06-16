16a67c1b0f01bd531b6d43b29f59f2340b8f4d8f
David <davmaguinap@uch.pe>
2023-06-16 11:29:56 -0500

    subiendo el proyecto de laravel jetstream

---
+++ resources/views/livewire/nav/show-posts.blade.php
@@ -0,0 +1,4 @@
+<div>
+    {{-- In work, do what you enjoy. --}}
+    <h1>Hola mundo dentro de la carpeta nav</h1>
+</div>
---
+++ resources/views/livewire/show-posts.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <h1>Hola Mundo</h1>
+    {{$name}}
+</div>
